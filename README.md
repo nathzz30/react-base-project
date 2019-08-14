10044173154534447299b5b3a951c9fbed03c2b3
Nathaly Ramirez Persia <nathzzpersia@gmail.com>
2019-08-14 14:26:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-base-project
+This is a base project made with React, Wedpack, Babel, etc. Is a setup enviroment for coding using react
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const App = () => {
+  return (
+    <div>
+      <h1>Holasss Bebessss</h1>
+      <p>React here!</p>
+    </div>
+  );
+};
+export default App;
+ReactDOM.render(<App />, document.getElementById("app"));
